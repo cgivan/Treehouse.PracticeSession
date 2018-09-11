975f6d0696016ce0f2cfc475b60e9f1fe8beb0ac
Chris Givan <chrisgivan502@gmail.com>
2018-09-11 14:39:37 -0400

    Practice Validation and Exceptions

---
+++ Treehouse.PracticeSession/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treehouse.PracticeSession
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bool keepGoing = true;
+
+            while(true)
+            {
+                Console.WriteLine("Enter a number between 1 and 1000: ");
+                string entry = Console.ReadLine();
+
+
+                if (entry.ToLower() == "quit")
+                {
+                    break;
+                }
+
+                try
+                {
+                    double number = double.Parse(entry);
+                    if (number <= 0)
+                    {
+                        Console.WriteLine("Please enter a number greater than 0.");
+                        continue;
+                    }
+                    else if (number > 1000)
+                    {
+                        Console.WriteLine("Please enter a number less than or equal to 1000.");
+                        continue;
+                    }
+                    else
+                    {
+                        double result = number * number;
+                        Console.WriteLine(number + " multiplied by itself is equal to " + result + ".");
+                    }
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("'" + entry + "' is not a number!");
+                }
+            }
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
